3bc754a130169d99d060174daa63a0b1d9ee8f1a
Saeed Dashti <saeed.da1994@gmail.com>
2025-08-09 22:49:38 +0330

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'testApp';
+
 }
