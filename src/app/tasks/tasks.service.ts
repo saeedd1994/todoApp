286c590fbea57e4fdfe01fd0690a93cc258cb398
Saeed Dashti <saeed.da1994@gmail.com>
2025-10-17 18:43:58 +0330

    save tasks using ls

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,6 +29,13 @@ export class TasksService {
     },
   ]
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string){
     return this.tasks.filter((task)=> task.userId === userId);
   }
@@ -43,10 +50,15 @@ export class TasksService {
         dueDate: taskData.date,
       }
     )
+    this.saveTask()
   }
 
   removeTask(id: string) {
-    this.tasks = this.tasks.filter((task)=> task.id !== id)
+    this.tasks = this.tasks.filter((task)=> task.id !== id);
+    this.saveTask();
   }
 
+  private saveTask(){
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
